74e96ab6c815d6f4b74eba885586121262be9144
Daisuke Sakurai <d.sakurai0@gmail.com>
2025-07-21 11:51:53 +0000

    Plane Nix

--- project/jupyter-lab-nix.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# cd to the root directory
-cd "$WORKSPACE_FOLDER"
-
-# Run jupyter lab through nix-develop
-./nix-scripts/nix-develop.sh --command bash -c 'jupyter lab --no-browser'
